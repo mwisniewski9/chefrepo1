56cacb8de92518bbfd684ff51424d7b513fc462b
Michael Wisniewski <mwisniewski9@gmail.com>
2015-03-17 01:04:58 -0700

    added apache,splunkand splunkasdfa cookbooks

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "apache2" do 
+	action :install
+end
+
+service "apache2" do
+	action [:enable, :start]
+end
+
+template "/var/www/html/index.html" do
+	source "index.html.erb"
+	mode "0644"
+end
